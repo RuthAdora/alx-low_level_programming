ada9b50084dbac90d8852bfdb3722ad872082b62
RuthAdora <adoraruth01@gmail.com>
2023-02-16 01:08:31 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int i;
 long int t;
 long long int n;
 float f;
+
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(t));
