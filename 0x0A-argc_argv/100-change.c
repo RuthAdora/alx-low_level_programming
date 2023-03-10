4981bd600bbeba841d79b94359e27755eb0bef12
RuthAdora <adoraruth01@gmail.com>
2023-03-10 08:21:51 -0800

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 #include <stdlib.h>
@@ -19,64 +18,63 @@ int main(int argc, char *argv[])
 
 {
 
-int num, j, result;
+	int num, j, result;
 
-int coins[] = {25, 10, 5, 2, 1};
-
-
-
-if (argc != 2)
-
-{
-
-printf("Error\n");
-
-return (1);
-
-}
+	int coins[] = {25, 10, 5, 2, 1};
 
 
 
-num = atoi(argv[1]);
+	if (argc != 2)
 
-result = 0;
+	{
 
+		printf("Error\n");
 
+		return (1);
 
-if (num < 0)
-
-{
-
-printf("0\n");
-
-return (0);
-
-}
-
-j = 0;
-
-for (j < 5 && num >= 0; j++)
-
-{
-
-while 
-	(num >= coins[j])
-
-{
-
-result++;
-
-num -= coins[j];
-
-}
-
-}
+	}
 
 
 
-printf("%d\n", result);
+	num = atoi(argv[1]);
 
-return (0);
+	result = 0;
+
+
+
+	if (num < 0)
+
+	{
+
+		printf("0\n");
+
+		return (0);
+
+	}
+
+
+
+	for (j = 0; j < 5 && num >= 0; j++)
+
+	{
+
+		while (num >= coins[j])
+
+		{
+
+			result++;
+
+			num -= coins[j];
+
+		}
+
+	}
+
+
+
+	printf("%d\n", result);
+
+	return (0);
 
 }
 
